777420bd65dc65bb123fc41035c3829b036b912f
198HIM <87613491+198HIM@users.noreply.github.com>
2022-04-06 23:24:30 +0800

    Demo1
    Demo

---
+++ Demo/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+import javax.print.event.PrintEvent;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        public void sortNums(){
+            int[][] nums = new int[][]{{1,2}, {3,4}, {7,8}, {5,6}};
+            //1. 传统写法
+            Arrays.sort(nums, Arrays::compare);
+       
+    }
+}
+}
